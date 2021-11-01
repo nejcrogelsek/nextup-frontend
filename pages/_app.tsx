435a62b1d5e4627cf2179132a01a1842326182dc
Nejc <nejcrogelsek0@gmail.com>
2021-11-01 21:39:54 +0100

    eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import "tailwindcss/tailwind.css";
-import "../styles/global.css";
-import ProgressBar from "@badrap/bar-of-progress";
-import Router from "next/router";
+import 'tailwindcss/tailwind.css';
+import '../styles/global.css';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
 
 const progress = new ProgressBar({
 	size: 4,
-	color: "#2f3c7e",
-	className: "z-50",
+	color: '#2f3c7e',
+	className: 'z-50',
 	delay: 100,
 });
 
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,23 @@ import Homepage from '../components/home/Home'
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
 			<Head>
 				<title>Nextup</title>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Header />
 
 			<Homepage />
-			<footer className="flex items-center justify-center w-full h-24 border-t">
+			<footer className='flex items-center justify-center w-full h-24 border-t'>
 				<a
-					className="flex items-center justify-center"
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
+					className='flex items-center justify-center'
+					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+					target='_blank'
+					rel='noopener noreferrer'
 				>
 					Powered by{' '}
-					<img src="/logo.svg" alt="Vercel Logo" className="h-4 ml-2" />
+					<img src='/logo.svg' alt='Vercel Logo' className='h-4 ml-2' />
 				</a>
 			</footer>
 			<Footer />
